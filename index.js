646a40655715d35cb10098ddcf726dfb07f78d76
Matteo Contrini <matteo@contrini.it>
2023-05-12 17:28:17 +0200

    Initial version

---
+++ index.js
@@ -0,0 +1,1 @@
+import '@fontsource/mulish/variable.css'
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        '*.html'
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'tbp': '#005FFF',
+                'azzurrino': '#F9FBFF'
+            }
+        },
+        fontFamily: {
+            sans: ['MulishVariable', 'sans-serif'],
+        }
+    },
+    plugins: [],
+}
